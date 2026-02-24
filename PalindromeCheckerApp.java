e6deb6c5653689f700e121ceb06c7ab3fc303514
hamsinigoel <hamsini4010@gmail.com>
2026-02-24 15:39:49 +0530

    UC6 Completed

--- PalindromeCheckerApp.java
+++ PalindromeCheckerApp.java
@@ -1,25 +1,30 @@
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Stack;
 
 public class PalindromeCheckerApp {
 
     public static void main(String[] args) {
-        String input = "noon";
+        String input = "civic";
+
+        Queue<Character> queue = new LinkedList<>();
         Stack<Character> stack = new Stack<>();
 
         for (char c : input.toCharArray()) {
+            queue.add(c);
             stack.push(c);
         }
 
         boolean isPalindrome = true;
 
-        for (char c : input.toCharArray()) {
-            if (c != stack.pop()) {
+        while (!queue.isEmpty()) {
+            if (queue.remove() != stack.pop()) {
                 isPalindrome = false;
                 break;
             }
         }
 
-        System.out.println("Palindrome Checker App - UC5");
+        System.out.println("Palindrome Checker App - UC6");
         System.out.println("Input String: " + input);
 
         if (isPalindrome) {
@@ -27,6 +32,7 @@ public class PalindromeCheckerApp {
         } else {
             System.out.println("Result: The string is not a palindrome.");
         }
-        System.out.println("Exiting UC5 flow...");
+
+        System.out.println("Exiting UC6 flow...");
     }
 }
